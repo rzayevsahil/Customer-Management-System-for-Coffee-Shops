1a3a6c712bec49d18dd0a802f88c7e8177c750c8
Sahil Rzayev <sahilrzayev2000@gmail.com>
2021-01-31 21:37:53 +0400

    Add project files.

---
+++ customer management system for coffee shops/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace customer_management_system_for_coffee_shops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
