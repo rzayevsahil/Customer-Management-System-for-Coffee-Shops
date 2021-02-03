848eb205e1f6c55084305beac66138c5142aff49
Sahil Rzayev <sahilrzayev2000@gmail.com>
2021-02-03 22:33:29 +0400

    The project has been updated

--- customer management system for coffee shops/Adapters/MernisServiceAdapter.cs
+++ customer management system for coffee shops/Adapters/MernisServiceAdapter.cs
@@ -4,15 +4,26 @@ using MernisServiceReference;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace customer_management_system_for_coffee_shops.Adapters
 {
     public class MernisServiceAdapter : ICustomerCheckService
     {
+        
+
         public bool CheckIfRealPerson(Customer customer)
         {
-            KPSPublicSoapClient client = new KPSPublicSoapClient();
-            return client.TCKimlikNoDogrulaAsync(customer.NationalityId, customer.FirstName.ToUpper(), customer.LastName.ToUpper(), customer.DateOfBirth.Year);
+            
+             //return client.TCKimlikNoDogrulaAsync(customer.NationalityId, customer.FirstName, customer.LastName, customer.DateOfBirth.Year);
+            return TaskAsync(customer).Result;
+        }
+
+        public static async Task <bool> TaskAsync(Customer customer)
+        {
+            KPSPublicSoapClient client = new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap12);
+            var durum = await client.TCKimlikNoDogrulaAsync(Convert.ToInt64(customer.NationalityId), customer.FirstName, customer.LastName, customer.DateOfBirth.Year);
+            return durum.Body.TCKimlikNoDogrulaResult;
         }
     }
 }
